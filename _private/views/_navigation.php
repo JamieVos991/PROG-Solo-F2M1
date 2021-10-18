1a36d9e68acc8469e6585aba7472152c0126a989
Jamie <31694@ma-web.nl>
2021-10-18 15:47:19 +0200

    Login en logout knop in layout zetten

--- _private/views/_navigation.php
+++ _private/views/_navigation.php
@@ -1,7 +1,11 @@
 <ul>
     <li>
-        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Home</a>
-            <a href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( 'register.form' ) ): ?> class="active"<?php endif ?>>Registreren</a>
-                <a href="<?php echo url( 'login.form' ) ?>"<?php if ( current_route_is( 'login.form' ) ): ?> class="active"<?php endif ?>>Login</a>
+        <a href="<?php echo url('home') ?>" <?php if (current_route_is('home')) : ?> class="active" <?php endif ?>>Home</a>
+            <a href="<?php echo url('register.form') ?>" <?php if (current_route_is('register.form')) : ?> class="active" <?php endif ?>>Registreren</a>
+                <?php if (isLoggedIn()) : ?>
+                    <a href="<?php echo url('logout') ?>">Uitloggen</a>
+                <?php else : ?>
+                    <a href="<?php echo url('login.form') ?>" <?php if (current_route_is('login.form')) : ?> class="active" <?php endif ?>>Inloggen</a>
+                <?php endif; ?>
     </li>
 </ul>
