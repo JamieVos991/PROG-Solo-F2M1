3b7983aa6c6a726a6aaf9fd45af97da930b929b8
Jamie <31694@ma-web.nl>
2021-10-28 00:45:04 +0200

    Landen sorteren op alfabet

--- _private/controllers/CoronaController.php
+++ _private/controllers/CoronaController.php
@@ -26,6 +26,9 @@ class CoronaController
 
 		// JSON omzetten in een array met json_decode()
 		$countries = json_decode($json, true);
+		usort($countries, function($a, $b){
+			return strcmp($a['Country'], $b['Country']);
+		});
 
 		$template_engine = get_template_engine();
 		echo $template_engine->render('corona_index', ['countries' => $countries] );
